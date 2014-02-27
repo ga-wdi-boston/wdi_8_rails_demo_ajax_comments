7abe049fe02bb882238f8f6d2d9c2a1df76befb0
David Fisher <tibbon@gmail.com>
2014-02-27 11:17:52 -0500

    Comments showing on screen

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
+  CommentApp.$commentsDiv = $('#all_comments');
+
   $.ajax({
     url: '/comments',
     type: 'get',
     dataType: 'json'
   })
   .done(function(data) {
-    console.log(data);
+    CommentApp.renderAllComments(data);
   });
 });
+
+CommentApp = {
+}
+
+CommentApp.renderAllComments = function(comments) {
+  var numComments = comments.length, i = 0;
+  for(; i < numComments; i++) {
+    CommentApp.renderComment(comments[i]);
+  }
+};
+
+CommentApp.renderComment = function(comment) {
+  var $commentDiv = $('<div>', {text: comment.content});
+  this.$commentsDiv.append($commentDiv);
+}
